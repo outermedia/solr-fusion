bdbdc91d13a468e065814692b98250902ccbdb12
sballmann <sba@outermedia.de>
2014-06-11 12:52:20 +0200

    Simple default implementation for lists.

---
+++ src/main/java/org/outermedia/solrfusion/adapter/ClosableListIterator.java
@@ -0,0 +1,46 @@
+package org.outermedia.solrfusion.adapter;
+
+import org.outermedia.solrfusion.response.ClosableIterator;
+
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by ballmann on 6/11/14.
+ */
+public class ClosableListIterator<T> implements ClosableIterator<T>
+{
+    private Iterator<T> listIt;
+
+    public ClosableListIterator(List<T> list)
+    {
+        this.listIt = list.iterator();
+    }
+
+    @Override
+    public void close()
+    {
+        listIt = null;
+    }
+
+    @Override
+    public boolean hasNext()
+    {
+        return listIt.hasNext();
+    }
+
+    @Override
+    public T next()
+    {
+        return listIt.next();
+    }
+
+    /**
+     * This method always throws a runtime exception ("not supported").
+     */
+    @Override
+    public void remove()
+    {
+        throw new RuntimeException("Not supported");
+    }
+}
